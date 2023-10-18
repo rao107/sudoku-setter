72a5412d6e395b2f8234f6d2229cc867aa504ae5
Anirudh Rao <rao107@purdue.edu>
2023-10-17 23:38:19 -0400

    Created sudoku grid

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-import { Int, And, Solver, Context } from 'z3-solver';
+import { init } from 'z3-solver';
 
-const context = new Context('main');
-
-const x = Int.const('x');
-
-const solver = new Solver();
-solver.add(And(x.ge(0), x.le(9)));
-console.log(solver.check());
+init();
