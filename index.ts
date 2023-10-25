6842b5f4f44762fd8572c6995b9ab58f2812ec1f
Anirudh Rao <rao107@purdue.edu>
2023-10-24 20:48:26 -0400

    Sudoku can now solve!

--- index.ts
+++ index.ts
@@ -4,31 +4,26 @@ declare global {
   interface Window { z3Promise: ReturnType<typeof z3.init>; }
 }
 
-// Used in getValue to cache the HTMLInputElements
-let getValueBlocks: {[index:string]:HTMLInputElement} = {};
-function getValue(x:number, y:number):number{
-  if (x < 1 || x > 9) {
-    throw new Error(`Invalid value of ${x} for x`);
+const inputGrid: HTMLInputElement[][] = Array.from(Array(9), () => new Array(9));
+for (let i = 0; i < 9; i++) {
+  for (let j = 0; j < 9; j++) {
+    inputGrid[i][j] = <HTMLInputElement>document.getElementById(`i${i}${j}`);
   }
-  if (y < 1 || y > 9) {
-    throw new Error(`Invalid value of ${y} for y`);
+}
+
+function getGrid(): number[][] {
+  const grid: number[][] = Array.from(Array(9), () => new Array(9));
+  for (let i = 0; i < 9; i++) {
+    for (let j = 0; j < 9; j++) {
+      grid[i][j] = parseInt(inputGrid[i][j].value);
+    }
   }
-  let key = `i${x}${y}`;
-  if (key in getValueBlocks) {
-    return parseInt(getValueBlocks[key].value);
-  }
-  let elem = (<HTMLInputElement>document.getElementById(key));
-  if (elem === null || elem.value === "") {
-    return 0;
-  }
-  // Cache because getElementById is slow
-  getValueBlocks[`${x}${y}`] = elem;
-  return parseInt(elem.value);
+  return grid;
 }
 
 // Grabs the constraints div and gets the id's of all checked children inputs 
 function getConstraints():string[]{
-  let out:string[] = [];
+  let out: string[] = [];
   document.getElementById("constraints")
     ?.querySelectorAll("input")
     ?.forEach(child=>{
@@ -43,84 +38,65 @@ window.z3Promise = z3.init();
 // so other scripts can Z3 - they just need to `await window.z3Promise`
 // this script must be before those scripts on the page
 
-
-let runBtn = document.getElementById('run')
-
+let runBtn = document.getElementById("run")
 if (runBtn === undefined || runBtn === null) {
   console.error("no run btn");
 } else {
-  runBtn.addEventListener('click', async event => {
+  runBtn.addEventListener("click", async () => {
     console.log(getConstraints());
 
-    // We can likely put most of the checking logic in here
-    //  I just copied what they had for Z3 and shoved it in here
-    //  to make sure it wasn't just available at page run
-    let { Z3 } = await window.z3Promise;
-    let config = Z3.mk_config();
-    let ctx = Z3.mk_context_rc(config);
-    Z3.del_config(config);
+    const sudokuGrid: number[][] = getGrid();
+    console.log(sudokuGrid);
 
-    let command = `
-      (declare-const a Int)
-      (declare-fun f (Int Bool) Int)
-      (assert (< a 10))
-      (assert (< (f a true) 100))
-      (check-sat)
-      (get-model)
-    `;
-    console.log(await Z3.eval_smtlib2_string(ctx, command));
-    Z3.del_context(ctx);
-    console.log('### running the high-level API')
-    // use the high-level API:
     let { Context } = await window.z3Promise;
-    let { Solver, Int } = Context('main');
+    let { Solver, Int, Distinct } = Context("main");
     let solver = new Solver();
-    let x = Int.const('x');
-    solver.add(x.add(5).eq(9));
+
+    let z3Grid: z3.Arith<"main">[][] = Array.from(Array(9), () => new Array(9));
+    for (let i = 0; i < 9; i++) {
+      for (let j = 0; j < 9; j++) {
+        z3Grid[i][j] = Int.const(`x_${i}${j}`);
+        if (1 <= sudokuGrid[i][j] && sudokuGrid[i][j] <= 9) {
+          solver.add(z3Grid[i][j].eq(sudokuGrid[i][j]));
+        } else {
+          solver.add(z3Grid[i][j].ge(1).and(z3Grid[i][j].le(9)));
+        }
+      }
+    }
+
+    // 1-9horiz
+    for (let i = 0; i < 9; i++) {
+      solver.add(Distinct(...z3Grid[i]));
+    }
+
+    // 1-9vert
+    for (let i = 0; i < 9; i++) {
+      solver.add(Distinct(...z3Grid.map(row => row[i])))
+    }
+
+    // 1-9nonet
+    // you could also probably use distinct here too but idk
+    for (let i = 0; i < 9; i++) {
+      for (let j = 0; j < 9; j++) {
+        for (let k = j + 1; k < 9; k++) {
+          let i_0 = (Math.floor(i / 3) * 3) + Math.floor(j / 3);
+          let j_0 = ((i % 3) * 3) + (j % 3);
+          let i_1 = (Math.floor(i / 3) * 3) + Math.floor(k / 3);
+          let j_1 = ((i % 3) * 3) + (k % 3);
+          solver.add(z3Grid[i_0][j_0].neq(z3Grid[i_1][j_1]));
+        }
+      }
+    }
+
     console.log(await solver.check());
-    console.log('x is', solver.model().get(x).toString());
+    
+    const resultGrid = Array.from(Array(9), () => new Array(9));
+    for (let i = 0; i < 9; i++) {
+      for (let j = 0; j < 9; j++) {
+        // i hate this parseInt weirdness but idk how to get an integer directly
+        resultGrid[i][j] = parseInt(solver.model().get(z3Grid[i][j]).toString());
+      }
+    }
+    console.log(resultGrid);
   });
 }
-
-
-(async () => {
-  // use the low-level API:
-  console.log('### running the low-level API')
-  let { Z3 } = await window.z3Promise;
-  let config = Z3.mk_config();
-  let ctx = Z3.mk_context_rc(config);
-  Z3.del_config(config);
-
-  for (let y = 1; y <= 9; y++) {
-    let o = ""
-    for (let x = 1; x <= 9; x++) {
-      o += getValue(x,y)
-    }
-    console.log(o)
-  }
-
-  let command = `
-    (declare-const a Int)
-    (declare-fun f (Int Bool) Int)
-    (assert (< a 10))
-    (assert (< (f a true) 100))
-    (check-sat)
-    (get-model)
-  `;
-  console.log(await Z3.eval_smtlib2_string(ctx, command));
-  Z3.del_context(ctx);
-
-
-
-  console.log('### running the high-level API')
-  // use the high-level API:
-  let { Context } = await window.z3Promise;
-  let { Solver, Int } = Context('main');
-  let solver = new Solver();
-  let x = Int.const('x');
-  solver.add(x.add(5).eq(9));
-  console.log(await solver.check());
-  console.log('x is', solver.model().get(x).toString());
-})().catch(e => {
-  console.error(e);
-});
