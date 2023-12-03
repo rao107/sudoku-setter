7a5195cf35b7b6fadfba47c821a4e9437ceef782
Anirudh Rao <rao107@purdue.edu>
2023-12-03 03:37:26 -0500

    Moved logic outside of solve lambda

--- index.ts
+++ index.ts
@@ -28,7 +28,121 @@ window.z3Promise = z3.init();
 // so other scripts can Z3 - they just need to `await window.z3Promise`
 // this script must be before those scripts on the page
 
-let runBtn = document.getElementById("run")
+/**
+ * Numbers in a row must not be the same
+ */
+function addRowConstraint(solver : z3.Solver<"main">, z3Grid : z3.Arith<"main">[][], Distinct : (...args: z3.CoercibleToExpr<"main">[]) => z3.Bool<"main">) {
+  for (let i = 0; i < 9; i++) {
+    solver.add(Distinct(...z3Grid[i]));
+  }
+}
+
+/**
+ * Numbers in a column must not be the same
+ */
+function addColConstraint(solver : z3.Solver<"main">, z3Grid : z3.Arith<"main">[][], Distinct : (...args: z3.CoercibleToExpr<"main">[]) => z3.Bool<"main">) {
+  for (let i = 0; i < 9; i++) {
+    solver.add(Distinct(...z3Grid.map(row => row[i])));
+  }
+}
+
+/**
+ * Numbers in a nonet must not be the same
+ */
+function addNonetConstraint(solver : z3.Solver<"main">, z3Grid : z3.Arith<"main">[][], Distinct : (...args: z3.CoercibleToExpr<"main">[]) => z3.Bool<"main">) {
+  for (let i = 0; i < 9; i += 3) {
+    for (let j = 0; j < 9; j += 3) {
+      let squares = Array.from({length: 9}, (_, k) => k).map((k) => {
+        return [i + Math.floor(k / 3), j + k % 3];
+      });
+      solver.add(Distinct(...squares.map(([row, col]) => z3Grid[row][col])));
+    }
+  }
+}
+
+/**
+ * All squares must not be equal to the offset squares globally
+ */
+function addOffsetConstraint(solver : z3.Solver<"main">, z3Grid : z3.Arith<"main">[][], offsets : [number, number][]) {
+  for (let i = 0; i < 9; i++) {
+    for (let j = 0; j < 9; j++) {
+      let squares =
+        offsets
+        .map(([i_0, j_0]) => [i + i_0, j + j_0])
+        .filter(([i_0, j_0]) => (0 <= i_0) && (i_0 < 9) && (0 <= j_0) && (j_0 < 9));
+      squares.forEach(([row, col]) => {
+        solver.add(z3Grid[i][j].neq(z3Grid[row][col]));
+      });
+    }
+  }
+}
+
+/**
+ * Given squares must strictly increase
+ */
+function addIncreasingConstraint(solver : z3.Solver<"main">, z3Grid : z3.Arith<"main">[][], squares : [number, number][]) {
+  for (let i = 0; i < squares.length - 1; i++) {
+    solver.add(z3Grid[squares[i][1]][squares[i][0]].lt(z3Grid[squares[i+1][1]][squares[i+1][0]]));
+  }
+}
+
+/**
+ * Given pair must be exactly a given difference from each other
+ */
+function addExactDifferenceConstraint(solver : z3.Solver<"main">, z3Grid : z3.Arith<"main">[][], pair : [number, number][], diff : number, Or : (...args: (boolean | z3.Bool<"main">)[]) => z3.Bool<"main">) {
+  if (pair.length == 2) {
+    // Each pair is [[x1,y1], [x2,y2]]
+    solver.add(Or(
+      z3Grid[pair[0][1]][pair[0][0]].sub(z3Grid[pair[1][1]][pair[1][0]]).eq(diff),
+      z3Grid[pair[1][1]][pair[1][0]].sub(z3Grid[pair[0][1]][pair[0][0]]).eq(diff)
+    ));
+  }
+}
+
+/**
+ * Given pair must be at least a given difference from each other
+ */
+function addAtLeastDifferenceConstraint(solver : z3.Solver<"main">, z3Grid : z3.Arith<"main">[][], pair : [number, number][], diff : number, Or : (...args: (boolean | z3.Bool<"main">)[]) => z3.Bool<"main">) {
+  if (pair.length == 2) {
+    solver.add(Or(
+      z3Grid[pair[0][1]][pair[0][0]].sub(z3Grid[pair[1][1]][pair[1][0]]).ge(diff),
+      z3Grid[pair[0][1]][pair[0][0]].sub(z3Grid[pair[1][1]][pair[1][0]]).le(-diff)
+    ));
+  }
+}
+
+/**
+ * Given pair must be at most a given difference from each other
+ */
+ function addAtMostDifferenceConstraint(solver : z3.Solver<"main">, z3Grid : z3.Arith<"main">[][], pair : [number, number][], diff : number, Or : (...args: (boolean | z3.Bool<"main">)[]) => z3.Bool<"main">) {
+  if (pair.length == 2) {
+    solver.add(Or(
+      z3Grid[pair[0][1]][pair[0][0]].sub(z3Grid[pair[1][1]][pair[1][0]]).le(diff),
+      z3Grid[pair[0][1]][pair[0][0]].sub(z3Grid[pair[1][1]][pair[1][0]]).ge(-diff)
+    ));
+  }
+}
+
+/**
+ * Given Kropki pairs must have one square be a double of another
+ */
+function addKropkiDoubleConstraint(solver : z3.Solver<"main">, z3Grid : z3.Arith<"main">[][], kkd : [number, number][], Or : (...args: (boolean | z3.Bool<"main">)[]) => z3.Bool<"main">) {
+  // Each kropki is [[x1,y1], [x2,y2]]
+  let fst = z3Grid[kkd[0][1]][kkd[0][0]];
+  let snd = z3Grid[kkd[1][1]][kkd[1][0]];
+  solver.add(Or(
+    fst.eq(1).and(snd.eq(2)),
+    fst.eq(2).and(snd.eq(1)),
+    fst.eq(2).and(snd.eq(4)),
+    fst.eq(3).and(snd.eq(6)),
+    fst.eq(4).and(snd.eq(2)),
+    fst.eq(4).and(snd.eq(8)),
+    fst.eq(6).and(snd.eq(3)),
+    fst.eq(8).and(snd.eq(4))
+  ));
+}
+
+let runBtn = document.getElementById("run");
 if (runBtn === undefined || runBtn === null) {
   console.error("no run btn");
 } else {
@@ -41,7 +155,7 @@ if (runBtn === undefined || runBtn === null) {
     console.log(sudokuGrid);
 
     let { Context } = await window.z3Promise;
-    let { Solver, Int, Distinct, /*Sum,*/ Or } = Context("main");
+    let { Solver, Int, Distinct, Or, If, And } = Context("main");
     let solver = new Solver();
 
     let z3Grid: z3.Arith<"main">[][] = Array.from(Array(9), () => new Array(9));
@@ -55,126 +169,61 @@ if (runBtn === undefined || runBtn === null) {
         }
       }
     }
-
     // 1-9horiz
     if (constraints.includes('1-9horiz')) {
-      for (let i = 0; i < 9; i++) {
-        solver.add(Distinct(...z3Grid[i]));
-      }
+      addRowConstraint(solver, z3Grid, Distinct);
     }
-
     // 1-9vert
     if (constraints.includes('1-9vert')) {
-      for (let i = 0; i < 9; i++) {
-        solver.add(Distinct(...z3Grid.map(row => row[i])));
-      }
+      addColConstraint(solver, z3Grid, Distinct);
     }
-
     // 1-9nonet
     if (constraints.includes('1-9nonet')) {
-      for (let i = 0; i < 9; i += 3) {
-        for (let j = 0; j < 9; j += 3) {
-          let squares = Array.from({length: 9}, (_, k) => k).map((k) => {
-            return [i + Math.floor(k / 3), j + k % 3];
-          });
-          solver.add(Distinct(...squares.map(([row, col]) => z3Grid[row][col])));
-        }
-      }
+      addNonetConstraint(solver, z3Grid, Distinct); 
     }
-
     // antiking
     if (constraints.includes('antiking')) {
-      let offsets = [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]];
-      for (let i = 0; i < 9; i++) {
-        for (let j = 0; j < 9; j++) {
-          let squares =
-            offsets
-            .map(([i_0, j_0]) => [i + i_0, j + j_0])
-            .filter(([i_0, j_0]) => (0 <= i_0) && (i_0 < 9) && (0 <= j_0) && (j_0 < 9));
-          squares.forEach(([row, col]) => {
-            solver.add(z3Grid[i][j].neq(z3Grid[row][col]));
-          });
-        }
-      }
+      addOffsetConstraint(solver, z3Grid, [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]]);
     }
-
     // antiknight
     if (constraints.includes('antiknight')) {
-      let offsets = [[-2, -1], [-2, 1], [-1, -2], [-1, 2], [1, -2], [1, 2], [2, -1], [2, 1]];
-      for (let i = 0; i < 9; i++) {
-        for (let j = 0; j < 9; j++) {
-          let squares =
-            offsets
-            .map(([i_0, j_0]) => [i + i_0, j + j_0])
-            .filter(([i_0, j_0]) => (0 <= i_0) && (i_0 < 9) && (0 <= j_0) && (j_0 < 9));
-          squares.forEach(([row, col]) => {
-            solver.add(z3Grid[i][j].neq(z3Grid[row][col]));
-          });
-        }
-      }
+      addOffsetConstraint(solver, z3Grid, [[-2, -1], [-2, 1], [-1, -2], [-1, 2], [1, -2], [1, 2], [2, -1], [2, 1]]);
     }
-
     // thermo
     savedConstraints.thermo.forEach(thermo => {
-      for (let i = 0; i < thermo.length - 1; i++) {
-        solver.add(z3Grid[thermo[i][1]][thermo[i][0]].lt(z3Grid[thermo[i+1][1]][thermo[i+1][0]]));
-      }
+      addIncreasingConstraint(solver, z3Grid, thermo);
     });
-
     // arrow
     savedConstraints.arrow.forEach(arrow => {
       // Each arrow is a list of [x,y] pairs
       // TODO
-      let temp = [];
-      for (let i = 1; i < arrow.length; i++) {
-        temp.push(z3Grid[arrow[i][1]][arrow[i][0]]);
-      }
+      //let temp = [];
+      //for (let i = 1; i < arrow.length; i++) {
+      //  temp.push(z3Grid[arrow[i][1]][arrow[i][0]]);
+      //}
       //solver.add(z3Grid[arrow[0][1]][arrow[0][0]].eq(Sum(...temp)));
-    })
 
+//      let sum = z3Grid[arrow[1][1]][arrow[1][0]];
+      //for (let i = 1; i < arrow.length; i++) {
+      //  sum.add(z3Grid[arrow[i][1]][arrow[i][0]]);
+      //}
+      //solver.add(z3Grid[arrow[0][1]][arrow[0][0]].eq(z3Grid[arrow[1][1]][arrow[1][0]].add(z3Grid[arrow[2][1]][arrow[2][0]]).add(z3Grid[arrow[3][1]][arrow[3][0]])));
+    });
     // kropki
     savedConstraints.kropkiAdjacent.forEach(kka => {
-      // Each kropki is [[x1,y1], [x2,y2]]
-      solver.add(Or(
-        z3Grid[kka[0][1]][kka[0][0]].sub(z3Grid[kka[1][1]][kka[1][0]]).eq(1),
-        z3Grid[kka[1][1]][kka[1][0]].sub(z3Grid[kka[0][1]][kka[0][0]]).eq(1)
-      ));
-    })
+      addExactDifferenceConstraint(solver, z3Grid, kka, 1, Or);
+    });
     savedConstraints.kropkiDouble.forEach(kkd => {
-      // Each kropki is [[x1,y1], [x2,y2]]
-      let fst = z3Grid[kkd[0][1]][kkd[0][0]];
-      let snd = z3Grid[kkd[1][1]][kkd[1][0]];
-      solver.add(Or(
-        fst.eq(1).and(snd.eq(2)),
-        fst.eq(2).and(snd.eq(1)),
-        fst.eq(2).and(snd.eq(4)),
-        fst.eq(3).and(snd.eq(6)),
-        fst.eq(4).and(snd.eq(2)),
-        fst.eq(4).and(snd.eq(8)),
-        fst.eq(6).and(snd.eq(3)),
-        fst.eq(8).and(snd.eq(4))
-      ));
-    })
-
+      addKropkiDoubleConstraint(solver, z3Grid, kkd, Or);
+    });
     // german whispers
     savedConstraints.germanWhispers.forEach(whisper => {
       // Each whisper is [x,y] array
-      if (whisper.length < 2) {
-        // do nothing
-      } else {
-        for (let i = 0; i < whisper.length - 1; i++) {
-          solver.add(Or(
-            z3Grid[whisper[i][1]][whisper[i][0]].sub(z3Grid[whisper[i+1][1]][whisper[i+1][0]]).ge(5),
-            z3Grid[whisper[i][1]][whisper[i][0]].sub(z3Grid[whisper[i+1][1]][whisper[i+1][0]]).le(-5)
-          ));
-        }
-        solver.add(Or(
-          z3Grid[whisper[whisper.length-1][1]][whisper[whisper.length-1][0]].sub(z3Grid[whisper[whisper.length-2][1]][whisper[whisper.length-2][0]]).ge(5),
-          z3Grid[whisper[whisper.length-1][1]][whisper[whisper.length-1][0]].sub(z3Grid[whisper[whisper.length-2][1]][whisper[whisper.length-2][0]]).le(-5),          
-        ));
+      for (let i = 0; i < whisper.length - 1; i++) {
+        let pair = [whisper[i], whisper[i + 1]];
+        addAtLeastDifferenceConstraint(solver, z3Grid, pair, 5, Or);
       }
-    })
-
+    });
     if (await solver.check() === "sat") {
       hideLoadingScreen();
       // Was SAT so tell the output
@@ -192,6 +241,17 @@ if (runBtn === undefined || runBtn === null) {
       hideLoadingScreen();
       alert("Was unsat or unknown");
     }
+  });
+}
+
+let numSolBtn = document.getElementById("num-sol");
+if (numSolBtn === undefined || numSolBtn === null) {
+  console.error("no num-sol btn");
+} else {
+  numSolBtn.addEventListener("click", async () => {
+    showLoadingScreen();
+    console.log("test");
+    hideLoadingScreen();
   });
 }
 
@@ -294,3 +354,7 @@ addEventListener('load', event => {
   console.log("Load");
   loadFromLocalStorage();
 })
+
+function Sum(arg0: z3.Arith<"main">): z3.CoercibleToExpr<"main"> {
+  throw new Error('Function not implemented.');
+}
